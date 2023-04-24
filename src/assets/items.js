6f9813949019465790c2eaca359ee5b90115ce7b
Striver <harshjain2720@gmail.com>
2023-04-24 17:11:51 +0530

    completed project

---
+++ src/assets/items.js
@@ -0,0 +1,98 @@
+import love_babbar from "../assets/love_babbar.jpg";
+import striver from "../assets/striver.png";
+import fraz from "../assets/fraz.jpg";
+
+const items = [
+  {
+    title: "Array",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA",
+  },
+  {
+    title: "String",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://youtu.be/Wdjr6uoZ0e0",
+  },
+  {
+    title: "Matrix",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://youtu.be/1CdolnvxLs0",
+  },
+  {
+    title: "Search & Sort",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsTp4pb_WBRahP1tnipLuX9qM",
+  },
+  {
+    title: "Linked Lists",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsTr54_TH_NK4ibFojS4mmQA6",
+  },
+  {
+    title: "Binary Tree",
+    subtitle: "Raj Vikramaditya",
+    image: striver,
+    link: "https://www.youtube.com/playlist?list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk",
+  },
+  {
+    title: "BST",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsTpTAcxT0N5dhzMauZ0A1ZL8",
+  },
+  {
+    title: "Greedy",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://youtu.be/HZOUwKCKF5o",
+  },
+  {
+    title: "Recursion & Backtracking",
+    subtitle: "Mohammad Fraz",
+    image: fraz,
+    link: "https://www.youtube.com/playlist?list=PLjkkQ3iH4jy82KRn9jXeFyWzvX7sqYrjE",
+  },
+  {
+    title: "Stack & Queue",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsTrhXYYtx4z8-u8zA-DzuVsj",
+  },
+  {
+    title: "Heap",
+    subtitle: "Love Babbar",
+    image: love_babbar,
+    link: "https://www.youtube.com/playlist?list=PLDzeHZWIZsToMrLX5-7qNyS6Db9VwTSvm",
+  },
+  {
+    title: "Trie",
+    subtitle: "Raj Vikramaditya",
+    image: striver,
+    link: "https://www.youtube.com/playlist?list=PLgUwDviBIf0pcIDCZnxhv0LkHf5KzG9zp",
+  },
+  {
+    title: "Graph",
+    subtitle: "Raj Vikramaditya",
+    image: striver,
+    link: "https://www.youtube.com/playlist?list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn",
+  },
+  {
+    title: "Dynamic Programming",
+    subtitle: "Raj Vikramaditya",
+    image: striver,
+    link: "https://www.youtube.com/playlist?list=PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY",
+  },
+  {
+    title: "Bit Manipulation",
+    subtitle: "Raj Vikramaditya",
+    image: striver,
+    link: "https://www.youtube.com/playlist?list=PLN4aKSfpk8TQq8ySZsy266qAWBnR2sN0G",
+  },
+];
+
+export default items;
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "../css/skeleton.css";
+import "../css/normalize.css";
+import "../css/components.css";
+
+function Footer() {
+  return (
+    <div className="Footer container">
+      <p>
+        Made with{" "}
+        <span role="img" aria-label="heart">
+          {" "}
+          ❤️{" "}
+        </span>
+        &nbsp;by CodersVault
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
